cb05c047265e76064053b5c578d5d7cbb4edff47
snareboi <justcube740@gmail.com>
2026-01-04 19:52:44 -0800

    Added special conditions, changed useEffect and simplified the choice render
    An array is used for special conditions. checkSpecialConditions() and modifyConditions() are used to update choice options depending on the state of conditions and change the state of conditions depending on if a scene is selected respectfully.
    
    UseEffect is triggered anytime the currSection is updated. This is to make the choice array smaller and separated by sections in a certain chapter.
    
    The choice callback is used rather than all fields of the callback. ChangeScene() was updated to reflect that. The first if conditions was also added to make removing certain choices easier. For example, if you visited a place, you wouldn't visit it again, so the choice for it should be removed. So, if the remove boolean is true for the option, the object will be replaced with a blank string and won't be rendered on the map function in subsequent renders.

--- app/Game/ChapterOne/Intro/Scene.js
+++ app/Game/ChapterOne/Intro/Scene.js
@@ -3,8 +3,17 @@ import { client } from "@/sanity/lib/client";
 import imageUrlBuilder from '@sanity/image-url';
 import Image from "next/image";
 import { PortableText } from "next-sanity";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { createChoices } from "./choices";
+import { Howler } from "howler";
+
+// const ambience = new Howl({
+//   src:["/audio/Scary Intro.m4a"],
+//   loop: true,
+//   volume: 0.5,
+// })
+
+// ambience.play();
 
 const builder = imageUrlBuilder(client);
 export default function Scene() {
@@ -18,14 +27,24 @@ export default function Scene() {
     const [images, setImages] = useState(null);
     const [currImage, changeImage] = useState(0);
 
+    
 
     const pictureStyle = {
       opacity: 0.8
     }
+
+
+    let conditions = [
+      true, //saw Mort's notebook
+      1, //the game you chose (1 = JackBox, 2 = Telestrations, 3 = Poker)
+      false, //Mitch gives Marlin his bed
+      false, //know in depth Marlin's situation with the group
+      false, //offered Alicia a job
+    ]
     
     useEffect (() => {
         async function getScenes() {
-            const query = `*[_type == "scene"] | order(id) {
+            const query = `*[_type == "scene" && section == $currSection] | order(id) {
                 id,
                 chapter,
                 section,
@@ -33,7 +52,7 @@ export default function Scene() {
                 "slug":slug.current
             }`;
             
-            const content = await client.fetch(query);
+            const content = await client.fetch(query, {currSection});
             updateText(content);
         }
 
@@ -48,22 +67,99 @@ export default function Scene() {
         }
         getScenes();
         getPictures();
-    }, [React.useRef()]);
+    }, [currSection]);
 
-    function changeScene(newScene, newChoice, newImage) {
-      if(newImage >= 0) {
-        changeImage(newImage);
+
+    function changeScene(choice) {
+      let column = parseInt(choice.key);
+      if(choice.remove) {
+        choices[currChoices][column] = ""; 
       }
-      updateScene(newScene);
-      updateChoices(newChoice);
       
-      if(newScene == 5) {
-        choices[3][0] = "";
-      } else if(newScene == 14) {
-        choices[3][1] = "";
+      if(choice.newImage >= 0) {
+        changeImage(choice.newImage);
+      }
+      
+      updateScene(choice.newScene);
+      updateChoices(choice.newChoice);
+      modifyConditions(choice.newScene);
+      checkSpecialCondition(choice.newScene);
+      if(choice.newSection != null) {
+        updateSection(choice.newSection);
+        modifyChoices(createChoices(choice.newSection));
+      }
+    }
+
+
+    //checks if a certain scene is picked to modify a condition
+    function modifyConditions(scene) {
+      if(currSection == "Intro") {
+        if (scene == 32) {
+          conditions[0] = true;
+        }
+      } else if (currSection == "Park") {
+        if (scene == 24) {
+          conditions[1] = 2;
+        } else if (scene == 25) {
+          conditions[1] = 3;
+        }
+      } else if (currSection == "Cabin") {
+        if (scene == 7) {
+          conditions[3] = true;
+        } else if (scene == 8) {
+          conditions[2] = true;
+          conditions[3] = true;
+        }
       }
       
     }
+
+    //all of the special condition events happen here
+    function checkSpecialCondition(scene) {
+      if(currSection == "Intro") {
+        if(conditions[0]) {
+          choices[20][0] = {
+              label: "Ask about Mort's notebook",
+              newScene: 27,
+              newChoice: 21,
+              newImage: -1,
+              removed: false,
+              key: "0"
+          };
+          choices[34][0].newChoice = 35;
+        } else if (choices[3][0] == "" && choices[3][1] == "" && choices[3][2] == "" && 
+          choices[3][3] == "" && choices[3][4] == "" && scene == 4) {
+          updateScene(42);
+          updateChoices(33);
+        }
+      } else if(currSection == "Park") {
+        if(conditions[0]) {
+          choices[8][0] = {
+              label: "Continue",
+              newScene: 16,
+              newChoice: 9,
+              newImage: -1,
+              remove: false,
+              key: "0"
+          }
+        }
+      } else if(currSection == "Cabin") {
+        if(conditions[0]) {
+          choices[1][3] = {
+            label: "Mort's notebook",
+            newScene: 10,
+            newChoice: 5,
+            newImage: -1,
+            remove: false,
+            key: "3"
+          }
+        }
+      }
+      
+    }
+
+
+
 
     return (
       <>
@@ -75,9 +171,9 @@ export default function Scene() {
             style={pictureStyle}
           />
         )}
-        {console.log(playerName)}
         <section className="bg-gray-200 ml-auto mr-auto mt-10 max-w-3xl min-w-1 opacity-90 text-black">
           <div className="mx-auto max-w-prose space-y-8 py-13 p-1 text-left">
+            <div className="fade-in" key={scene}>
               <article className="p-2 prose md:prose-md prose-primary mx-auto max-h-125 overflow-y-scroll">
                   <PortableText value={text ? replaceInBlocks(text[scene].text, playerName) : ""} components={[]} />
                   <br/>
@@ -85,12 +181,13 @@ export default function Scene() {
                     choice ? 
                       <div key={choice.key}>
                       <button key={choice.key} className="md:hover:underline cursor-pointer p-1 text-left text-red-500 font-bold" 
-                      onClick={() => changeScene(choice.newScene, choice.newChoice, choice.newImage)}>
+                      onClick={() => changeScene(choice)}>
                         {choice.label}
                       </button>
                     </div> : null
                   )}
               </article>
+              </div>
           </div>   
         </section>    
          
@@ -123,4 +220,6 @@ function replaceInBlocks(blocks, playerName) {
 function urlFor(source) {
  return builder.image(source);
 }
+
+
 
